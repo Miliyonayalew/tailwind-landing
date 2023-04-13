5651da6f197af9fed6b1296225b68ec41494a0e5
Miliyonayalew <miliayalew@gmail.com>
2023-04-13 21:58:39 +0300

    Add cards

--- src/App.jsx
+++ src/App.jsx
@@ -47,8 +47,68 @@ function App() {
         
         <h2 className='text-indigo-700 font-bold text-xl pb-1 mt-8 border-b border-indigo-300'>Advantages</h2>
 
-     
-    </div>
+        <section className='flex flex-col sm:flex-row sm:-mx-2'>
+          <div className='sm:w-1/3 mt-4'>
+            <div className='bg-white h-full p-8 cursor-pointer border-b-4 border-pink-500  rounded-lg flex flex-col items-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 sm:mx-2 sm:p-3 md:p-8'>
+              <div className='bg-gray-200 text-indigo-700 rounded-full w-16 p-2'>
+                <Css className="w-12 h-12 fill-current"/>
+              </div>
+              <div className='mt-4 font-bold text-gray-600 text-sm'>No Custom CSS</div>
+              <div className='text-center mt-2'>
+                Tailwind's advanced class extraction will leave your project free of custom CSS.
+              </div>
+            </div>
+          </div>
+          <div className='sm:w-1/3 mt-4'>
+            <div className='bg-white h-full p-8 border-b-4 cursor-pointer border-pink-500 rounded-lg flex flex-col items-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 sm:mx-2 sm:p-3 md:p-8'>
+              <div className='bg-gray-200 text-indigo-700 rounded-full w-16 p-2'>
+                <Developer className="w-12 h-12 fill-current"/>
+              </div>
+              <div className='mt-4 font-bold text-gray-600 text-sm'>Developer Experience</div>
+              <div className='text-center mt-2'>
+                Tailwind is designed with your happiness in mind. The ease of performing changes is refreshing.
+              </div>
+            </div>
+          </div>
+          <div className='sm:w-1/3 mt-4'>
+            <div className='bg-white h-full p-8 border-b-4 cursor-pointer border-pink-500 rounded-lg flex flex-col items-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300 sm:mx-2 sm:p-3 md:p-8'>
+              <div className='bg-gray-200 text-indigo-700 rounded-full w-16 p-2'>
+                <Mobile className="w-12 h-12 fill-current"/>
+              </div>
+              <div className='mt-4 font-bold text-gray-600 text-sm'>Mobile Friendly</div>
+              <div className='text-center mt-2'>
+                Modern applications demand mobile-friendly & Tailwind's responsive modifiers make it easy.
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+      <section className="bg-gray-800 py-12 mt-12 w-full flex flex-col items-center border-t-2 border-black">
+        <h2 className="font-bold text-xl text-gray-400">Sign up to download the free PDF</h2>
+        <form className="max-w-xl w-full text-gray-200 mt-4 px-8 md:px-0" action="#" method="post">
+          <div className="flex flex-col md:flex-row md:-mx-2">
+            <label for="first_name" className="sr-only">First Name</label>
+            <input className="bg-gray-700 mt-4 border border-gray-600 rounded-lg py-2 px-4 w-full md:mx-2" type="text" placeholder="First name" id="first_name"/>
+                        
+            <label for="last_name" className="sr-only">Last Name</label>
+            <input className="bg-gray-700 mt-4 border border-gray-600 rounded-lg py-2 px-4 w-full md:mx-2" type="text" placeholder="Last name" id="last_name"/>
+          </div>
+          
+          <div className="flex flex-col md:flex-row  md:-mx-2">
+            <label for="email" className="sr-only">Email address</label>
+            <input className="bg-gray-700 mt-4 border border-gray-600 rounded-lg py-2 px-4 w-full md:mx-2" type="email" placeholder="john@email.com" id="email"/>
+                          
+            <label for="title" className="sr-only">Title</label>
+            <input className="bg-gray-700 mt-4 border border-gray-600 rounded-lg py-2 px-4 w-full md:mx-2" type="text" placeholder="Your title" id="title"/>
+          </div>
+
+          <div className="flex flex-col md:flex-row md:-mx-2">
+            <button className="bg-blue-600 text-sm mt-4 rounded-lg py-2 px-4 w-full md:mx-2">Create account</button>
+          </div>
+        </form>
+      </section>
+     </main>
+       </div>
   )
 }
 
